945cda83048b3c703c4eb07d14411f15cc8ad2e8
n-kratz <80129545+n-kratz@users.noreply.github.com>
2024-01-19 15:35:31 -0500

    Update manage.py

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "veganapp.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "veganapp-railway.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
